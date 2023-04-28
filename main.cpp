f84d0ab0c7a4bebff71be19d59bcbcf8ff5a239f
nickt144 <nickdngtr@gmail.com>
2023-04-28 19:01:41 -0400

    Updated listPassword and insertPassword functions to work. Created createAccount function.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
 #include "sqlite/sqlite3.h"
+#include <sstream>
 using namespace std;
 
 bool listPassword(sqlite3* db,sqlite3_stmt* stmt, int id){
-    sqlite3_prepare_v2(db, "select pass from password where id = " + id + "", -1, &stmt, 0);
+    ostringstream oss;
+    oss << id;
+    cout << id;
+    string sql = "select pass from password where ID = " + oss.str();
+    sqlite3_prepare_v2(db, sql.c_str() , -1, &stmt, 0);
+
     const unsigned char* password;
     while(sqlite3_step(stmt) != SQLITE_DONE){
-        password = sqlite3_column_text(stmt,1);
-        cout << password;
+        password = sqlite3_column_text(stmt,0);
+        cout << password << "\n";
     }
     return true;
 }
 
-string insertPassword(sqlite3* db,sqlite3_stmt* stmt, int id , string password){
+string insertPassword(sqlite3 *db, int id, string password) {
     char* error;
     int rc;
-    string query = "insert into password VALUES (" + id + ", " + 'password'  +" );";
-    cout<< query;
+    string query = "INSERT INTO password VALUES (" + to_string(id) + ", '" +  password  +"');";
+    rc = sqlite3_exec(db, query.c_str(), NULL, NULL, &error);
+    if(rc != SQLITE_OK){
+        cout<< "error: " << error;
+    }
+}
+
+bool createAccount(sqlite3 *db, int id){
+    char* error;
+    int rc;
+    if(id == NULL){
+        id = 0;
+    }
+    string query = "INSERT INTO user VALUES (" + to_string(id) + ");";
     rc = sqlite3_exec(db, query.c_str(), NULL, NULL, &error);
     if(rc != SQLITE_OK){
         cout<< "error: " << error;
@@ -32,21 +50,32 @@ int main() {
     sqlite3_stmt* stmt;
     sqlite3_open("passDB.db", &db);
 
-    cout << "Are you creating a new account?";
+    cout << "Are you creating a new account?"<< "\n";
     cin >> choice;
     if(choice){
-        IDnum  = sqlite3_exec(db, "SELECT COUNT(1) FROM user;", NULL,NULL,&error);
-        if(IDnum != SQLITE_OK){
-            cout<< "error: " << error;
+        string sql = "select count(*) from user";
+        sqlite3_prepare_v2(db, sql.c_str() , -1, &stmt, 0);
+        while(sqlite3_step(stmt) != SQLITE_DONE){
+            IDnum = sqlite3_column_int(stmt,0);
+            createAccount(db, IDnum );
+            cout<< "This is your ID: " +  to_string(IDnum)<< "\n";
         }
-        cout<< "This is your ID: " +  IDnum;
     }else{
         cout << "What is your ID?";
         cin >> IDnum;
         listPassword(db, stmt, IDnum);
     }
-    cout << "";
 
+
+
+
+
+
+
+
+
+
+/*
     rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS user(ID INT);", NULL,NULL,&error);
     if(rc != SQLITE_OK){
         cout<< "error: " << error;
@@ -56,7 +85,7 @@ int main() {
         cout<< "error: " << error;
     }
 
-
+*/
 
 
     sqlite3_close(db);
